739c60e28be611508e911dad4bd7270e3599c545
Gernot Messow <gmessow@insys-locks.de>
2025-04-02 10:20:08 +0200

    codecompanion: added gemini default model and keymaps

--- config/codecompanion.nix
+++ config/codecompanion.nix
@@ -1,10 +1,26 @@
-{ ... }:
+{ config, lib, createKeymaps, ... }:
 {
   plugins = {
     codecompanion = {
       enable = true;
 
       settings = {
+        adapters = {
+          gemini = {
+            __raw = ''
+              function()
+                return require('codecompanion.adapters').extend('gemini', {
+                  schema = {
+                    model = {
+                      default = 'gemini-2.5-pro-exp-03-25',
+                    },
+                  },
+                })
+              end
+            '';
+          };
+        };
+
         strategies = {
           agent = { adapter = "gemini"; };
           chat = { adapter = "gemini"; };
@@ -13,4 +29,14 @@
       };
     };
   };
+
+  keymaps = lib.mkIf config.plugins.codecompanion.enable (createKeymaps {
+    "n" = [
+      ["<leader>aa" "<cmd>CodeCompanionActions<CR>" "Open CodeCompanion Action Palette" { silent = true; }]
+      ["<leader>ak" "<cmd>CodeCompanionChat toggle<CR>" "Toggle CodeCompanion Chat" { silent = true; }]
+    ];
+    "v" = [
+      ["ga" "<cmd>CodeCompanionChat Add<cr>" "Add visually selected text to the current CodeCompanion chat" { silent = true; }]
+    ];
+  });
 }
