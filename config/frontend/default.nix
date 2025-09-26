c237a9a55f5f108b98258c7369d348ebe99fc028
Gernot Messow <gmessow@insys-locks.de>
2025-09-26 12:08:39 +0200

    frontend: removed volar, just use ts_ls plus vue typescript plugin

--- config/frontend/default.nix
+++ config/frontend/default.nix
@@ -1,17 +1,33 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   config = {
     plugins = {
       lsp = {
         enable = true;
 
-        servers = {
+        servers = let
+          vue_plugin = {
+            name = "@vue/typescript-plugin";
+            location = "${lib.getBin pkgs.vue-language-server}/lib/language-tools/packages/language-server";
+            languages = ["vue"];
+            configNamespace = "typescript";
+          };
+        in {
           ts_ls = {
             enable = true;
             onAttach.function = ''
               client.server_capabilities.documentFormattingProvider = false
             '';
+            filetypes = ["typescript" "javascript" "javascriptreact" "typescriptreact" "vue"];
+            extraOptions = {
+              init_options = {
+                plugins = [vue_plugin];
+              };
+            };
           };
-          volar.enable = true;
         };
       };
 
